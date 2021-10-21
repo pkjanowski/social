66ae5b27d8d4bd0fbeb98a54437363aefcd32231
pkjanowski <paweljanowski6@gmail.com>
2021-10-21 13:16:37 +0200

    update

--- dj_studybud/base/admin.py
+++ dj_studybud/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Room, Topic, Message     #dodanie do paneli admina interfejsu do room 1/2
 # Register your models here.
+
+admin.site.register(Room)       #dodanie do paneli admina interfejsu do room 2/2
+admin.site.register(Topic)       #dodanie do paneli admina interfejsu do room 2/2
+admin.site.register(Message)       #dodanie do paneli admina interfejsu do room 2/2
--- dj_studybud/base/views.py
+++ dj_studybud/base/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name': 'Lets learn python'},
-    {'id':2, 'name': 'Design with me'}, 
-    {'id':3, 'name': 'Frontend developers'},
-]
+# rooms = [
+#     {'id':1, 'name': 'Lets learn python'},
+#     {'id':2, 'name': 'Design with me'}, 
+#     {'id':3, 'name': 'Frontend developers'},
+# ]
+
+
 
 
 #tworzenie nowej podstrony (widoku) podajemy jako argument HTTP request
 #wersja właściwa: render - renderujemy coś zapisanego w pliku html
 def home(request):
+    rooms = Room.objects.all()
     context =  {'rooms': rooms}
     return render(request, 'base/home.html', context)  #podajemy rooms do template
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
